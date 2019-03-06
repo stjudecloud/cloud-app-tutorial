8591093a23051635d786418920c8243e6dc7572e
Arthur Chiao <chiaoarthur@gmail.com>
2019-03-06 14:09:20 -0600

    Update FastQC example app to reflect changes in documentation

--- examples/dx-fastqc-example-app/src/dx-fastqc-example-app.sh
+++ examples/dx-fastqc-example-app/src/dx-fastqc-example-app.sh
@@ -6,21 +6,21 @@ main() {
     echo "Value of bam_file: '$bam_file'"
 
     # Downloads file from project to virtual machine workspace
-    dx download "$bam_file" -o bam_file
+    dx download "$bam_file"
 
     # Creating output directory for FastQC
     mkdir ~/fastqc-out
 
     # Runs FastQC on BAM file
-    /FastQC/fastqc -f bam bam_file -o ~/fastqc-out
+    /FastQC/fastqc "$bam_file_name" -o ~/fastqc-out
 
     # Renames the FastQC reports to include the BAM file prefix
-    mv ~/fastqc-out/*.html ~/fastqc-out/"$bam_file_prefix"_fastqc.html
-    mv ~/fastqc-out/*.zip ~/fastqc-out/"$bam_file_prefix"_fastqc.zip
+    mv ~/fastqc-out/*.html ~/fastqc-out/fastqc-report.html
+    mv ~/fastqc-out/*.zip ~/fastqc-out/fastqc-report.zip
 
     # Uploads the respective HTML and Zip file
-    fastqc_html=$(dx upload ~/fastqc-out/"$bam_file_prefix"_fastqc.html --brief)
-    fastqc_zip=$(dx upload ~/fastqc-out/"$bam_file_prefix"_fastqc.zip --brief)
+    fastqc_html=$(dx upload ~/fastqc-out/fastqc-report.html --brief)
+    fastqc_zip=$(dx upload ~/fastqc-out/fastqc-report.zip --brief)
 
     # Adds and formats appropriate output variables for your app
     dx-jobutil-add-output fastqc_html "$fastqc_html" --class=file
